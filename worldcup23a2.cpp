8d59070b41658e180be228d50c95ebcd9828fad8
shayro9 <shayro13@gmail.com>
2023-01-04 18:50:04 +0200

    .

--- worldcup23a2.cpp
+++ worldcup23a2.cpp
@@ -341,10 +341,12 @@ StatusType world_cup_t::buy_team(int teamId1, int teamId2)
     Team* buying = &team1->data;
     Team* bought = &team2->data;
 
-    Player* buying_root = buying->getRootPlayer(); 
+    Player* buying_root = buying->getRootPlayer();
     Player* bought_root = bought->getRootPlayer();
 
     players.Unite(buying_root->getId(),bought_root->getId());
+    buying->addPoints(bought->getPoints());
+    buying->addAbility(bought->getAbility());
     remove_team(teamId2);
 
 
