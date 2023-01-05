58c599b21a97641e679ace21b1771580da421472
shayro9 <shayro13@gmail.com>
2023-01-05 16:56:49 +0200

    ץ

--- cmake-build-debug/CMakeFiles/wet2_mvne.dir/TeamAbility.cpp.obj.d
+++ cmake-build-debug/CMakeFiles/wet2_mvne.dir/TeamAbility.cpp.obj.d
@@ -1,3 +1,1 @@
-CMakeFiles/wet2_mvne.dir/TeamAbility.cpp.obj: \
- C:\Users\amida\CLionProjects\untitled1\wet2_mvne\TeamAbility.cpp \
- C:\Users\amida\CLionProjects\untitled1\wet2_mvne\TeamAbility.h
+C:/Users/shayr/Documents/GitHub/wet2_mvne/TeamAbility.h
